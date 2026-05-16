e5a506ea17eab9788e13ad39c3b199dc8fb724d7
Shashank Trivedi <81739538+strivedi4u@users.noreply.github.com>
2026-05-17 05:24:35 +0530

    Fix soft delete in BoardService

--- backend/src/main/java/com/taskboard/service/BoardService.java
+++ backend/src/main/java/com/taskboard/service/BoardService.java
@@ -77,12 +77,10 @@ public class BoardService {
         Board board = boardRepository.findByIdAndDeletedFalse(boardId)
                 .orElseThrow(() -> new BoardNotFoundException(boardId));
 
-        // BUG #1: This is doing hard delete instead of soft delete
-        // The test expects: board.setDeleted(true); boardRepository.save(board);
-        // But the implementation is:
-        boardRepository.delete(board);
+        board.setDeleted(true);
+        boardRepository.save(board);
 
         eventPublisher.publishEvent(BoardEvent.deleted(boardId));
-        log.info("Deleted board: {}", boardId);
+        log.info("Soft-deleted board: {}", boardId);
     }
 }
